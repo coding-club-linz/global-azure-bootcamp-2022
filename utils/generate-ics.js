61a5b8e4808c649aba6a54cf218f11cfea864b1b
Karin Huber <karin@software-architects.at>
2024-04-03 16:14:51 +0200

    Schedule

--- utils/generate-ics.js
+++ utils/generate-ics.js
@@ -11,26 +11,26 @@ fs.readFile(path.join('.', 'data', 'sessionize.json'), 'utf8', function (err, se
                 .split('\r').join('')
                 .split('\n').join('\\n');
 
-            let shortTitle;
-            if (session.questionAnswers) {
-                const shortAnswerQuestion = session.questionAnswers.filter(qa => qa.questionId == 52401);
-                if (shortAnswerQuestion.length > 0) {
-                    shortTitle = shortAnswerQuestion[0].answerValue;
-                }
-            }
+            // let shortTitle;
+            // if (session.questionAnswers) {
+            //     const shortAnswerQuestion = session.questionAnswers.filter(qa => qa.questionId == 52401);
+            //     if (shortAnswerQuestion.length > 0) {
+            //         shortTitle = shortAnswerQuestion[0].answerValue;
+            //     }
+            // }
 
-            if (!shortTitle) {
-                continue;
-            }
+            // if (!shortTitle) {
+            //     continue;
+            // }
 
             const ics = icsTemplateContents
                 .split('[TITLE]').join('GAA: ' + session.title)
                 .replace('[DESCRIPTION]', session.description)
                 .replace('[DTSTART]', session.startsAt.split('-').join('').split(':').join(''))
                 .replace('[DTEND]', session.endsAt.split('-').join('').split(':').join(''))
-                .replace('[URL]', 'https://globalazure.at/streaming/');
+                .replace('[URL]', 'https://globalazure.at/');
 
-            fs.writeFileSync(path.join('.', 'static', 'ics', shortTitle + '.ics'), ics);
+            fs.writeFileSync(path.join('.', 'static', 'ics', session.id + '.ics'), ics);
 
             // const redirectMd = '---\nsessionId: "' + session.id + '"\n---';
             // fs.writeFileSync(path.join('.', 'content', 'sessions', shortTitle + '.md'), redirectMd);
